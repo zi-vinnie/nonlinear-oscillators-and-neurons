ce8bf3c313517426576af737ca4de82b75c409b4
Grace Wigley <grace@wigley.org.uk>
2026-02-27 12:21:32 +0000

    Midpoint method

--- VanderPol.py
+++ VanderPol.py
@@ -7,6 +7,7 @@ def VanderPolModel(
     x0: float, y0: float, dt: float, endTime: float, params: ModelParams
 ) -> tuple[float, float]:
     positionValues = [np.array([x0, y0])]
+    positionValues2 = [np.array([x0, y0])]
     numSteps = int(endTime / dt)
 
     for i in range(numSteps):
@@ -25,8 +26,17 @@ def VanderPolModel(
         # Append the new position to the list of position values
         positionValues.append(np.array([x, y]))
 
+        x2, y2 = midpoint_step_2d( 
+            # positionValues[-1] indicates the last position, 
+            # 0 indicates the x values 
+            # 1 indicates the y values
+            positionValues2[-1][0], positionValues2[-1][1], dt, dx_dt, dy_dt
+        )
+        # Append the new position to the list of positive values
+        positionValues2.append(np.array([x2,y2]))
+
     # Return the position values as a numpy array
-    return np.array(positionValues)
+    return np.array(positionValues), np.array(positionValues2)
 
 
 # Main function
@@ -40,7 +50,7 @@ if __name__ == "__main__":
     x0 = 2
     y0 = 0
     dt = 0.1
-    endTime = 100
+    endTime = 33
 
     fig, axs = plt.subplots()
     # Run the Van der Pol model for each value of I
@@ -48,13 +58,16 @@ if __name__ == "__main__":
         # Create the parameters for the FitzHugh-Nagumo model
         params = ModelParams(0, mu, 0, 0)
         # Run the FitzHugh-Nagumo model for the current value of I
-        positionValues = VanderPolModel(
+        positionValues, positionValues2 = VanderPolModel(
             x0, y0, dt, endTime, params
         )
         # Plot the position values on a graph
         xValues = positionValues[:, 0]
         yValues = positionValues[:, 1]
-        axs.plot(xValues, yValues)
+        xValues2 = positionValues2[:, 0] 
+        yValues2 = positionValues2[:, 1]
+        #axs.plot(xValues, yValues)
+        axs.plot(xValues2, yValues2, linestyle = "--")
     # Save the figure
     # Check if the directory exists, if not create it
     if not os.path.exists("VanderPol"):
