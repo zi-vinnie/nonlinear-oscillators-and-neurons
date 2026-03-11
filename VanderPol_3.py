99c2651c5f16141ac9180dde77d96ebe616af33b
GraceWigley <grace@wigley.org.uk>
2026-03-11 18:20:20 +0000

    edited code

--- VanderPol_3.py
+++ VanderPol_3.py
@@ -4,53 +4,43 @@ import os
 from helpers import *
 from VanderPol import * 
 
-# Defining the parameters
-mu = 10
-params = ModelParams(0, mu, 0, 0)
+# Parameters of the system 
+mu = 2
 
-eulerPositionsExplore, midpointPositionsExplore = VanderPolModel(x0, y0, endTime / numSteps, numSteps, params)
-xValuesExplore = midpointPositionsExplore[:, 0]
-yValuesExplore = midpointPositionsExplore[:, 1]
+# Create a grod in the phase plane 
+x_vals = np.linspace(-2.5,2.5,25)
+y_vals = np.linspace(-2.5,2.5,25)
+X, Y = np.meshgrid(x_vals, y_vals) 
 
-if __name__ == "__main__":
-    # Define the parameters for the FitzHugh-Nagumo model
-    IValues = np.linspace(0, 2, 3)
-    muValues = np.linspace(0.1, 60, 5)
-    a = 0
-    b = 0
-    x0 = 2
-    y0 = 0
-    dt = 0.1
-    endTime = 100
-    xAxisValues = np.linspace(-2, 2, 100)
+# Vector field components 
+F = X - (1/3)*X**3 - Y 
+G = X/mu 
 
-    fig, axs = plt.subplots()
-    # Run the FitzHugh-Nagumo model for each value of I
-    for I in IValues:
-        # Create the parameters for the FitzHugh-Nagumo model
-        params = ModelParams(I, mu, a, b)
-        # Run the FitzHugh-Nagumo model for the current value of I
-        positionValues = VanderPolModel(
-            x0, y0, dt, endTime, params
-        )
-        # Plot the position values on a graph
-        xValues = positionValues[:, 0]
-        yValues = positionValues[:, 1]
-        axs.plot(xValues, yValues)
-        xNullcline = xNullclineWithParams(I)
-        xNullclineValues = xNullcline(xAxisValues)
-        axs.plot(xAxisValues, xNullclineValues, linestyle = "--", color = "red", label = f"x Nullcline I = {I}")
+# Normalise vectors to show direction only 
+magnitude = np.sqrt(F**2 + G**2) 
+F_hat = F / magnitude
+G_hat = G / magnitude
 
-    yNullcline = yNullclineWithParams(a, b)
-    yNullclineValues = yNullcline(xAxisValues)
-    axs.plot(xAxisValues, yNullclineValues, linestyle = "--", color = "blue", label = f"y Nullcline")
-    axs.legend()
-    axs.set_title("FitzHugh-Nagumo Model Phase Portrait")
-    axs.set_xlabel("X")
-    axs.set_ylabel("Y")
-    # Save the figure
-    # Check if the directory exists, if not create it
-    if not os.path.exists("FitzHughNagumo"):
-        os.makedirs("FitzHughNagumo")
+# Nullclines 
+def x_nullclineVP(x):
+    y = x - (1/3)*x**3
+    return y 
 
-    fig.savefig("FitzHughNagumo/fitzHughNagumo.png", dpi=3
+def y_nullclineVP(x):
+    x = 0
+    return 0 
+
+yXNullcline = np.array(x_nullclineVP(x_vals))
+x_vals_yN = np.linspace(0,0,25)
+
+plt.figure(figsize =(7, 7))
+# Plot the vector field 
+plt.plot(x_vals, yXNullcline, label = "x-nullcline")
+plt.plot(x_vals_yN, y_vals, label = "y-nullcline")
+plt.quiver(X, Y, F_hat, G_hat, color = "gray")
+plt.legend()
+plt.title("Phase plane mu = 2, with nullclines")
+plt.xlabel("x values")
+plt.ylabel("y values")
+plt.show()
+# plot x(t) against time and y(t) against time
